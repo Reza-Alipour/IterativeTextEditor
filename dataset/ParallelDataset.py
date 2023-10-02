b9e6c4887c7110cb8e8f717279350ce4775267f3
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-10-02 19:35:23 +0330

    Fix a minor bug

--- dataset/ParallelDataset.py
+++ dataset/ParallelDataset.py
@@ -547,10 +547,10 @@ class Paradetox(ParallelDataset):
 
 class IteraTeRV2(ParallelDataset):
     def __init__(self, task_type, prompts, **kwargs):
+        self.type = task_type
         super().__init__(**kwargs)
         self.prompts = prompts
         self.pattern = r'<S>(.*?)</S>'
-        self.type = task_type
         self.generated_ds = self.generate_dataset()
 
     def generate_dataset(self) -> DatasetDict:
