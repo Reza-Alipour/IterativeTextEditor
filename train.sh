a0dd6ed6164aa0254750463df1d3af8e7fc985f7
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-10-03 16:59:55 +0330

    Fix a minor issue in training script

--- train.sh
+++ train.sh
@@ -26,7 +26,7 @@ elif [ "$1" == "PA" ]; then
         --per_device_train_batch_size 32 \
         --per_device_eval_batch_size 32 \
         --output_dir output/ \
-        --deepspeed config/ds_config.json
+        --deepspeed configs/ds_config.json
 
 
 else
